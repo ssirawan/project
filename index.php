3eeb17db8e24ab9743e2fb95784cd56f476441b0
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-14 16:21:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
-/*
-$db = pg_connect("host=ec2-54-235-133-42.compute-1.amazonaws.com port=5432 dbname=dd9n7qflnjtae8 user=ynezixirsjupio password=8aa84c7e317c93709b62657ac0a26d2f8696df1eaee0f6bb108e6cd3a2ca4d22");
-echo $db;
-*/
 
+$REPLY_URL = 'https://api.line.me/v2/bot/message/reply';
 $ACCESS_TOKEN = 'vEcA9SC+uVHF+zBZZQod5Yp/fS2Xn+lUkqHKi1EE1OGXZjtGJlfwrKfkLFu+wOyVPGomLXbzjZOWaK7MQjJsJ3c0kPBhnDo2vxEdES6a2Kk8PnQNwJRLHbPslhqvzC1xk8lM8HLtnERPSG8oXBLNvwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $CHANNEL_SECRET = 'e33ac5e982da548d1c1984ac6a97a69e';
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
@@ -23,7 +20,9 @@ foreach ($events as $event)
   if($event->strlen(getText()) > 0)
   {
 	  $data = createNewRichmenu(getenv($ACCESS_TOKEN));
-	  $bot->replyMessage($event->getReplyToken(),$data);
+	  //$bot->replyMessage($event->getReplyToken(),$data);
+	  $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+	  $send_result = send_reply_msg($REPLY_URL, $POST_HEADER, $post_body);
 
   }
 
@@ -101,8 +100,9 @@ EOF;
   }
 }
 
-/*
-function create_richmenu($url, $post_header, $post_body)
+
+
+function send_reply_msg($url, $post_header, $post_body)
 {
  $ch = curl_init($url);
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -112,10 +112,9 @@ function create_richmenu($url, $post_header, $post_body)
  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
  $result = curl_exec($ch);
  curl_close($ch);
- return $result;
-	
+ return $result;	
 }
-*/
+
 
 
 
