d3caea20ebaca0582b11897160291c60e996bc4d
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-16 15:04:43 +0700

    Create sendMessage.php

---
+++ sendMessage.php
@@ -0,0 +1,46 @@
+<?php
+
+function sentMessage($post_url,$post_header,$jsonbody)
+{
+  $datareturned =[];
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $post_url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_HTTPHEADER => $post_header,
+    CURLOPT_POSTFIELDS => $post_body,
+    CURLOPT_FOLLOWLOCATION => 1));
+  
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+  
+  curl_close($curl);
+  
+  if($err)
+  {
+     $datareturned['result'] = 'E';
+     $datareturned['message'] = $err;
+  }
+  else
+  {
+    if($response== "{}")
+    {
+      $datareturned['result'] = 'S';
+      $datareturned['message'] = 'Success';
+    }
+    else
+    {
+      $datareturned['result'] = 'E';
+      $datareturned['message'] = $response;
+    }
+  }
+  
+  
+  return $datareturned;
+  
+}
+
+?>
