c5f4a864c6deb2652ef5f9e8818ef5d65988b6fe
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-17 22:08:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ foreach ($request_array['events'] as $event)
 	  //$bot->replyMessage($event->getReplyToken(),new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(createNewRichmenu(getenv($ACCESS_TOKEN))));
 	  $richmenu_id = create_rich_menu($RICH_URL,$ACCESS_TOKEN,$rich_obj_req); 
 	  // อันนี้ลอง post กลับไปที่ LINE แต่ใช้ฟังก์ชันคล้ายกับ send_reply_msg แต่return ค่าต่างกัน
-	  file_put_contents("php://stderr", "POST JSON ===> ".$richmenu_id);
+	  file_put_contents("php://stderr", "POST JSON ===> ".$richmenu_id['replyToken']);
   
 	
   if( strlen($richmenu_id) > 0 ) 
