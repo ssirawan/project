7207e3ef427508ef6c31ac8097c7e27f6af43473
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-17 15:41:33 +0700

    #real_project

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+
+$db = pg_connect(("host=ec2-107-22-162-8.compute-1.amazonaws.com port=5432 dbname=d8kmurmr59kdsg user=kdozgixaxyediw password=0fad69adabf7a1c52fec6765c9331e776845abe09fc5a3b7c9c5ae1ccc9f6531");
+
+?>
