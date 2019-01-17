16af6e640d1eb206f68cd5de6a0205c47fcf8008
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-17 11:51:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -63,19 +63,6 @@ echo "OK";
 
 
 
-function send_reply_msg($url, $post_header, $post_body)
-{
- $ch = curl_init($url);
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
- curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- $result = curl_exec($ch);
- curl_close($ch);
- return $result;	
-}
-
 function create_rich_menu($post_url, $post_header, $post_body)
 {
  $ch = curl_init($url);
