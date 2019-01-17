e8d5618e32168903ad746d0484c304d8eea6c3b2
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-17 13:30:27 +0700

    token

---
+++ TypeMessage.php
@@ -0,0 +1,15 @@
+<?php
+
+/*Function GET Token*/
+	function getTokenData()
+	{
+		$token = "vEcA9SC+uVHF+zBZZQod5Yp/fS2Xn+lUkqHKi1EE1OGXZjtGJlfwrKfkLFu+wOyVPGomLXbzjZOWaK7MQjJsJ3c0kPBhnDo2vxEdES6a2Kk8PnQNwJRLHbPslhqvzC1xk8lM8HLtnERPSG8oXBLNvwdB04t89/1O/w1cDnyilFU=";
+		return $token;
+	}
+
+
+
+
+
+
+?>
