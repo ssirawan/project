8c402367348c018096d02625cf2ecec979ad003e
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-17 22:01:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <?php
 require 'sendMessage.php';
 require 'TypeMessage.php';
-
-
 $RICH_URL = 'https://api.line.me/v2/bot/richmenu';
 $REPLY_URL = 'https://api.line.me/v2/bot/message/reply';
 $ACCESS_TOKEN = getTokenData(); 
 $CHANNEL_SECRET = 'b41da5ab3d233c34e1bffc5a75a63846';
 $POST_HEADER = array('Content-Type: application/json ; charset=UTF-8', 'Authorization: Bearer ' . $ACCESS_TOKEN, 'cache-control: no-cache');
-
 /*
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($ACCESS_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $CHANNEL_SECRET]);
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 */
-
 $request = file_get_contents('php://input');  
 $request_array = json_decode($request, true); 
-
 foreach ($request_array['events'] as $event)
 {
   
-	  
+	  $rich_object = []; 
 	  $reply_token = $event[0]['replyToken'];
 	  $rich_area = array(
 		  array('bounds'=> array( 'x'=>'0','y'=>'0','width' => 1254,'height' => 850 ), 'action' => array('type'=> 'postback', 'text' =>'ดูสินค้า')),
@@ -34,68 +28,58 @@ foreach ($request_array['events'] as $event)
 	  $rich_object = array('size'=> array('width'=>2500,'height'=>1686),'selected'=>true,
 			     'name'=>'menu','chatBarText'=>'menu','areas'=>  $rich_area );
 	  $rich_obj_req = json_encode($rich_object, JSON_UNESCAPED_UNICODE);
+	  //$bot->replyMessage($event->getReplyToken(),new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(createNewRichmenu(getenv($ACCESS_TOKEN))));
 	  $richmenu_id = create_rich_menu($RICH_URL,$ACCESS_TOKEN,$rich_obj_req); 
 	  // อันนี้ลอง post กลับไปที่ LINE แต่ใช้ฟังก์ชันคล้ายกับ send_reply_msg แต่return ค่าต่างกัน
-	  
-	  file_put_contents("php://stderr", "POST JSON ===> ".$richmenu_id['richMenuId']);
-	  //file_put_contents("php://stderr", "POST URL ===> ".$RICH_URL);
+	  file_put_contents("php://stderr", "POST JSON ===> ".$richmenu_id);
   
 	
-  
-        $msg = [
-		'replyToken' => $reply_token,
-		'messages'=> [[
+  if( strlen($richmenu_id) > 0 ) 
+  {
+        $msg = [[
 	'type'=>'text',
-	'text'=>$richmenu_id['richMenuId']
-	]]];
+	'text'=>$richmenu_id
+	]];
 	  
 	$reply_msg = json_encode($msg);  
-	$send_result = create_rich_menu($REPLY_URL, $POST_HEADER, $reply_msg);  
-
-  
-
+	$send_result = sentMessage($REPLY_URL, $POST_HEADER, $reply_msg);  
+  };
 	  
    
-
-
   }
-
-
 echo "OK";
-
-
-
 function create_rich_menu($post_url, $ACCESS_TOKEN , $post_body)
 {
- $ch = curl_init($post_url);
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+
+	$curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $post_url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $post_body,
+      CURLOPT_HTTPHEADER => array(
         "authorization: Bearer ".$ACCESS_TOKEN,
         "cache-control: no-cache",
         "content-type: application/json; charset=UTF-8",
-      ));
- curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
- curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      ),
+    ));
 
- $result = curl_exec($ch);
- $err = curl_error($ch);
+ 
+ $result = curl_exec($curl);
+ $err = curl_error($curl);
  	
- curl_close($ch);
+ curl_close($curl);
 	
  if ($err) {
-      return $err;
-   } else {
-    	return json_decode($result,true);
-   }	
-
+        return $err;
+    } else {
+    	return $result;
+    }	
 	
 	
 }
-
-
-
-
 ?>
