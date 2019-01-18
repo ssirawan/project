ce3b9acab3908a4e41491b19826f7c45f7006bb9
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-18 10:21:21 +0700

    Create setrichMenuDefault.php

---
+++ RichMenu/setrichMenuDefault.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'TypeMessage.php';
+
+$ACCCES_TOKEN = gettokendata();
+$richMenuId = 'richmenu-2e64f30b116cfd79224317814e696858';
+
+
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://api.line.me/v2/bot/user/all/richmenu/'.$richMenuId,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      //CURLOPT_POSTFIELDS => $post_body,
+      CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer ".$ACCESS_TOKEN,
+        "cache-control: no-cache",
+        "content-type: application/json; charset=UTF-8",
+      ),
+    ));
+ 
+ $result = curl_exec($curl);
+ $err = curl_error($curl);
+ 	
+ curl_close($curl);
+	
+ if ($err) {
+        var_dump($err);
+    } else {
+    	  var_dump($result);
+    }	
+}	
+
+file_put_contents("php://stderr", "POST JSON ===> $result);
+
+?>
