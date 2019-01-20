347c3cb8b8dc4b7766ce24d9864416fcc3fbbe04
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-20 11:08:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -134,23 +134,23 @@ $curl = curl_init();
 
 function set_richmenu_default($richMenuId,$ACCESS_TOKEN)
 {
-$curl = curl_init();
-	curl_setopt_array($curl, array(
-	      CURLOPT_URL => "https://api.line.me/v2/bot/user/all/richmenu/".$richMenuId,
-	      CURLOPT_RETURNTRANSFER => true,
-	      CURLOPT_ENCODING => "",
-	      CURLOPT_MAXREDIRS => 10,
-	      CURLOPT_TIMEOUT => 30,
-	      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	      CURLOPT_CUSTOMREQUEST => "POST",
-	      CURLOPT_POSTFIELDS => $post_body
-	      CURLOPT_HTTPHEADER => array(
+	$curl = curl_init();
+    	curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.line.me/v2/bot/user/all/richmenu/".$richMenuId,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $post_data,
+      CURLOPT_HTTPHEADER => array(
         "authorization: Bearer ".$ACCESS_TOKEN,
         "cache-control: no-cache",
         "content-type: application/json; charset=UTF-8",
       ),
     ));
-
+	
 	 $result = curl_exec($curl);
 	 $err = curl_error($curl);
 
